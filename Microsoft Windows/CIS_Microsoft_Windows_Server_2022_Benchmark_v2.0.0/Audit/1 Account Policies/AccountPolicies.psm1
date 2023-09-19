147ffe116121cbecc5ff4f28d426c2050ab9d484
Mike Hiersche <hierschemichael@gmail.com>
2023-09-19 14:30:43 -0400

    Adds var for testing FGPP and names each policy

--- Microsoft Windows/CIS_Microsoft_Windows_Server_2022_Benchmark_v2.0.0/Audit/1 Account Policies/AccountPolicies.psm1
+++ Microsoft Windows/CIS_Microsoft_Windows_Server_2022_Benchmark_v2.0.0/Audit/1 Account Policies/AccountPolicies.psm1
@@ -5,7 +5,7 @@
 function Test-PasswordHistory {
     [CmdletBinding()]
     param (
-        [Parameter()][bool]$FineGrainedPasswordPolicy
+        [Parameter()][bool]$FineGrainedPasswordPolicy = $true
     )
     Write-Verbose "This settings is required for Level 1 compliance"
     Write-Host "For compliance, password history should be set to 24 or more passwords remembered."
@@ -13,25 +13,32 @@ function Test-PasswordHistory {
     if ($PasswordPolicy.PasswordHistoryCount -lt "24") {
         $Message = "Password history is set to " + $PasswordPolicy.PasswordHistoryCount + " and does not meet the requirement. Increase the policy to 24 or greater."
         Write-Warning $Message
+        $result = $false
     } else {
         $Message = "Password history is set to " + $PasswordPolicy.PasswordHistoryCount + " and does meet the requirement."
         Write-Host $Message -ForegroundColor Green
+        $result = $true
     }
 
-    #if ($FineGrainedPasswordPolicy) {
-    $ADFineGrainedPasswordPolicy = Get-ADFineGrainedPasswordPolicy -filter *
-    $Message = "Checking " + $ADFineGrainedPasswordPolicy.count + " Fine Grained Password Policies."
-    Write-Verbose $Message
-    foreach ($FGPasswordPolicy in $ADFineGrainedPasswordPolicy) {
-        if ($FGPasswordPolicy.PasswordHistoryCount -lt "24") {
-            $Message = "The " + $FGPasswordPolicy.Name + " policy has the Password history set to " + $FGPasswordPolicy.PasswordHistoryCount + " and does not meet the requirement. Increase the policy to 24 or greater."
-            Write-Warning $Message
-        } else {
-            $Message = "The " + $FGPasswordPolicy.Name + " policy has the Password history set to "+ $FGPasswordPolicy.PasswordHistoryCount + " and does meet the requirement."
-            Write-Host $Message -ForegroundColor Green
+    if ($FineGrainedPasswordPolicy) {
+        $ADFineGrainedPasswordPolicy = Get-ADFineGrainedPasswordPolicy -filter *
+        $Message = "Checking " + $ADFineGrainedPasswordPolicy.count + " Fine Grained Password Policies."
+        Write-Verbose $Message
+        foreach ($FGPasswordPolicy in $ADFineGrainedPasswordPolicy) {
+            if ($FGPasswordPolicy.PasswordHistoryCount -lt "24") {
+                $Message = "The " + $FGPasswordPolicy.Name + " Fine Grained Password Policy has the Password history set to " + $FGPasswordPolicy.PasswordHistoryCount + " and does not meet the requirement. Increase the policy to 24 or greater."
+                Write-Warning $Message
+                $result = $false
+            } else {
+                $Message = "The `"" + $FGPasswordPolicy.Name + "`" Fine Grained Password Policy has the Password history set to "+ $FGPasswordPolicy.PasswordHistoryCount + " and does meet the requirement."
+                Write-Host $Message -ForegroundColor Green
+                $result = $true
+            }
+            $Message = "This policy is applied to `n" + $FGPasswordPolicy.AppliesTo
+            Write-Verbose $Message
         }
     }
-    #}
+    Return $result
 }
 
 Export-ModuleMember -Function Test-PasswordHistory
