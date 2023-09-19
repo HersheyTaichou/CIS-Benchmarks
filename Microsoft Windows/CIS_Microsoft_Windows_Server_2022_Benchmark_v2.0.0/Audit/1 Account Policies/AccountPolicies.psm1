bae2bf27e7d37d4cdbf30c0ac108202d46dd4c6a
Mike Hiersche <hierschemichael@gmail.com>
2023-09-19 14:14:53 -0400

    Adds Get-AdFineGrainedPasswordPolicy to 1.1.1

--- Microsoft Windows/CIS_Microsoft_Windows_Server_2022_Benchmark_v2.0.0/Audit/1 Account Policies/AccountPolicies.psm1
+++ Microsoft Windows/CIS_Microsoft_Windows_Server_2022_Benchmark_v2.0.0/Audit/1 Account Policies/AccountPolicies.psm1
@@ -1,25 +1,11 @@
 # This module is designed to provide functions that test for complaince with CIS Benchmarks Version 2.0.0 for Windows Server 2022
+# 1.1.1 (L1) Ensure 'Enforce password history' is set to '24 or more password(s)' (Automated)
 
-<#
-.SYNOPSIS
-1.1.1 (L1) Ensure 'Enforce password history' is set to '24 or more password(s)' (Automated)
 
-.DESCRIPTION
-Long description
-
-.PARAMETER FirstParam
-Parameter description
-
-.EXAMPLE
-An example
-
-.NOTES
-General notes
-#>
 function Test-PasswordHistory {
     [CmdletBinding()]
     param (
-        [Parameter()][String]$FirstParam
+        [Parameter()][bool]$FineGrainedPasswordPolicy
     )
     Write-Verbose "This settings is required for Level 1 compliance"
     Write-Host "For compliance, password history should be set to 24 or more passwords remembered."
@@ -29,8 +15,21 @@ function Test-PasswordHistory {
         Write-Warning $message
     } else {
         $message = "Password history is set to " + $PasswordPolicy.PasswordHistoryCount + " and does meet the requirement."
-        Write-Host $message
+        Write-Host $message -ForegroundColor Green
     }
+
+    #if ($FineGrainedPasswordPolicy) {
+    $ADFineGrainedPasswordPolicy = Get-ADFineGrainedPasswordPolicy -filter *
+    foreach ($PasswordPolicy in $ADFineGrainedPasswordPolicy) {
+        if ($PasswordPolicy.PasswordHistoryCount -lt "24") {
+            $message = "Password history is set to " + $PasswordPolicy.PasswordHistoryCount + " and does not meet the requirement. Increase the policy to 24 or greater."
+            Write-Warning $message
+        } else {
+            $message = "Password history is set to " + $PasswordPolicy.PasswordHistoryCount + " and does meet the requirement."
+            Write-Host $message -ForegroundColor Green
+        }
+    }
+    #}
 }
 
 Export-ModuleMember -Function Test-PasswordHistory
