4ab256edb272b4ba01060d6f05002c3b7e88b863
Mike Hiersche <hierschemichael@gmail.com>
2025-02-21 09:19:33 -0500

    Add class for the CISControls

--- CISBenchmarkAudit/Source/Classes/CISBenchmark.ps1
+++ CISBenchmarkAudit/Source/Classes/CISBenchmark.ps1
@@ -1,3 +1,22 @@
+<# 
+class CISControl {
+    [int]$Version # The control version
+    [string]$Control # The title of the control
+    [int]$ImplementationGroup # Level this control is applicable to
+
+    CISControl() { $this.Init(@{}) }
+
+    CISControl([hashtable]$Properties) { $this.Init($Properties) }
+
+    [void] Init([hashtable]$Properties) {
+        foreach ($Property in $Properties.Keys) {
+            $this.$Property = $Properties.$Property
+        }
+    }
+
+}
+#>
+
 class CISBenchmark {
     [string]$Number # The number of the benchmark
     [string]$Level # Level 1, 2 or Next Generation Windows Security
@@ -6,6 +25,7 @@ class CISBenchmark {
     [string]$Source # Where the setting was checked from
     [bool]$SetCorrectly # if it is set correctly
     $Setting # The current setting
+    #[CISControl]$CISControl
     hidden $Entry # The XML output of the setting
 
     CISBenchmark() { $this.Init(@{}) }
